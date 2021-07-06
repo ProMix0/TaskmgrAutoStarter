52c12312bbf2030f7c3c7dbc7dee8f4e54dea445
ProMix <misha.zavertkin@mail.ru>
2021-07-06 15:20:45 +0700

    Добавьте файлы проекта.

---
+++ TaskmgAutoStarter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace TaskmgAutoStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Process manager= Process.Start("taskmgr.exe");
+                Console.WriteLine("Started");
+                while (manager.MainWindowHandle.ToInt32() == 0) Thread.Sleep(10);
+                ShowWindow(manager.MainWindowHandle, 6);
+                manager.WaitForExit();
+            }
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    }
+}
